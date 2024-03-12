da688dcdb27a292f4da88e859189d9897bb5a02f
NTiger07 <falaleru@gmail.com>
2024-03-12 17:03:30 +0100

    Implemented Delete room route

--- hotel-backend/routes/rooms.js
+++ hotel-backend/routes/rooms.js
@@ -1,52 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Room = require('../models/Room') 
-
+const Room = require("../models/Room");
 
 // @desc     Add Room
 // @route    POST  "/rooms/add"
 
-router.post("/add",  async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
     await Room.create(req.body);
-    res.send("Room Added")
-    res.status(200)
+    res.send("Room Added");
+    res.status(200);
   } catch (error) {
     console.error(error);
     res.status(500);
   }
 });
 
+// @desc     Delete Room
+// @route    POST  "/rooms/add"
 
+router.post("/delete/:room_number", async (req, res) => {
+  try {
+    await Room.findOneAndDelete({
+      room_number: req.params.room_number,
+    });
+    res.send("Room Deleted");
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+});
 
 // @desc     Get all rooms
 // @route    GET  "/rooms/all"
 
-router.get("/all",  async (req, res) => {
+router.get("/all", async (req, res) => {
   try {
     const rooms = await Room.find();
-    res.send(rooms)
-    res.status(200)
+    res.send(rooms);
+    res.status(200);
   } catch (error) {
     console.error(error);
     res.status(500);
   }
 });
-
-
 
 // @desc     Get number of rooms
 // @route    GET  "/rooms/meta"
 
-router.get("/meta",  async (req, res) => {
+router.get("/meta", async (req, res) => {
   try {
     const rooms = await Room.find();
-    res.send(rooms.length)
-    res.status(200)
+    res.send(rooms.length);
+    res.status(200);
   } catch (error) {
     console.error(error);
     res.status(500);
   }
 });
 
-module.exports = router
+module.exports = router;
