b162d244537063bfe08a108c8faefb32360f7fc3
NTiger07 <falaleru@gmail.com>
2024-03-15 15:40:45 +0100

    Implemented Filter for rooms

--- hotel-frontend/src/components/Rooms/RoomsContainer.tsx
+++ hotel-frontend/src/components/Rooms/RoomsContainer.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 import RoomItem from "./RoomItem"
 
 const RoomsContainer = () => {
+  const [searchValue, setSearchValue] = useState("")
+  const [roomType, setRoomType] = useState("")
+
   return (
     <div className="pt-[2rem]">
       <span className="text-primary-red font-bold text-[3rem]">Rooms</span>
-      <div className="flex items-center">
-        <select name="diet">
+
+      <div className="FILTER flex items-center gap-[2rem] py-[3rem]">
+        <select value={roomType} onChange={(e) => setRoomType(e.target.value)} className="py-[.5rem] cursor-pointer px-[.6rem] text-[1.1rem] outline-none border rounded-md bg-transparent border-greys-etherium">
           <option value="">All</option>
           <option value="basic">Basic</option>
           <option value="family">Family</option>
@@ -14,13 +19,19 @@ const RoomsContainer = () => {
         </select>
 
         <div>
-          <input 
-          type="search" 
-          className="pl-[20px] w-[100%] text-[1.2rem] outline-none appearance-none"
-          placeholder="Search rooms"
+          <input
+            type="search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="pl-[20px] w-[120%] text-[1.2rem] py-[.5rem] outline-none appearance-none rounded-md border border-greys-etherium"
+            placeholder="Search rooms"
           />
         </div>
       </div>
+
+
+
+
       <div className="grid grid-cols-4 gap-y-[3rem] pb-[2rem] gap-x-3">
         <RoomItem />
         <RoomItem />
