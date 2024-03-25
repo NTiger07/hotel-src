ab5fa5c2b7f27d01360a9ad314127d51cfa5387b
NTiger07 <falaleru@gmail.com>
2024-03-25 19:54:45 +0100

    Dialog DONE ready for deployment

--- hotel-frontend/src/components/Bookings/BookingItem.tsx
+++ hotel-frontend/src/components/Bookings/BookingItem.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import dayjs from "dayjs";
 import { useMemo, useState } from "react";
 import { toast } from "react-toastify";
+import Dialog from '@mui/material/Dialog';
 
 type BookingProps = {
     id: string,
@@ -40,11 +41,37 @@ const BookingItem = (props: BookingProps) => {
     }
 
     return (
-        <div className="relative w-full flex items-center py-[1.3%] px-[3%]" style={setBackdropStyle}>
-            {isVisible && status == "cancelled" ? <div className="absolute right-0 z-50 top-[42%] shadow-lg bg-white rounded-lg p-[1rem] POPUP cursor-pointer" onClick={cancelAppointment}>
-                Cancel Appointment
-            </div> : null}
+        <div className="w-full flex items-center py-[1.3%] px-[3%]" style={setBackdropStyle}>
 
+            <Dialog
+                open={isVisible}
+                onClose={() => setIsVisible(false)}
+            >
+                <div className="p-[1.5rem] h-max flex flex-col gap-[1rem]">
+                    <h1 className="font-semibold text-[1.5rem] text-center">Cancel Booking</h1>
+                    <span className="font-medium text-[1.2rem]">Are you sure you want to cancel this booking?</span>
+                    <div className="flex items-center justify-center gap-[30px]">
+                        <button
+                            className="cursor-pointer py-[10px] px-[30px] bg-[transparent] rounded-3xs flex flex-row items-start justify-start border-[1px] border-solid border-primary-red"
+                            onClick={() => setIsVisible(false)}
+                        >
+                            <b className="relative text-base text-primary-red text-left">
+                                No
+                            </b>
+                        </button>
+
+                        <button
+                            className="cursor-pointer [border:none] py-[10px] px-[30px] bg-primary-red rounded-3xs flex flex-row items-start justify-start"
+                            onClick={cancelAppointment}
+                        >
+                            <b className="relative text-base text-whites-plain text-left">
+                                Yes
+                            </b>
+                        </button>
+                    </div>
+                </div>
+
+            </Dialog>
             <span className="w-[25%] text-[1.1rem]">{client}</span>
             <span className="w-[12%]">Room {room_number}</span>
             <div className="w-[15.5%] flex flex-col">
@@ -56,10 +83,10 @@ const BookingItem = (props: BookingProps) => {
             <span className="w-[15.5%]">{status[0].toUpperCase() + status.substring(1)}</span>
             <div className="relative w-[2.5%] flex justify-center">
                 <img
-                    src="/icons/double-arrow.svg"
-                    className="w-[22px] h-[20px] cursor-pointer"
+                    src="/icons/vectorX.svg"
+                    className={`w-[12px] h-[12px] cursor-pointer ${status == "in-progress" ? "" : "hidden"}`}
                     alt=""
-                    onClick={() => { setIsVisible(!isVisible) }}
+                    onClick={() => setIsVisible(true)}
                 />
             </div>
         </div >
