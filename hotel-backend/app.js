ed06e097a098ca2da227ccde309ea8dee3555a96
NTiger07 <falaleru@gmail.com>
2024-03-26 19:48:39 +0100

    Added hotelms origin

--- hotel-backend/app.js
+++ hotel-backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://hotelms.netlify.app"],
     credentials: true,
     methods: ["GET", "POST"],
     allowedHeaders: [
